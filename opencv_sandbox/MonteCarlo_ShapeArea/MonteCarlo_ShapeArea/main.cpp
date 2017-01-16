3a6e795ecc4a9d1425b8886c09ec0fc32a0015f4
Nightwalker495 <milan.ondrasovic@gmail.com>
2017-01-16 08:40:59 +0100

    Working version of changing image pixel values.

--- opencv_sandbox/MonteCarlo_ShapeArea/MonteCarlo_ShapeArea/main.cpp
+++ opencv_sandbox/MonteCarlo_ShapeArea/MonteCarlo_ShapeArea/main.cpp
@@ -6,49 +6,48 @@
 
 using namespace std;
 
-static const cv::String fileName = "rectangle.png";
+static const cv::String DEFAULT_WIN_NAME = "Default Image";
+static const cv::String MODIFIED_WIN_NAME = "Modified Image";
+
+static const cv::String IN_FILENAME = "rectangle.png";
+static const cv::String OUT_FILENAME = "rectangle_modified.png";
+
+static void modifyImage(cv::Mat& image)
+{
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			cv::Vec3b& color = image.at<cv::Vec3b>(i, j); // BGR model
+
+			if ((color[0] == 0) && (color[1] == 0) && (color[2] == 0)) {
+				color[0] = 255;
+				color[1] = 0;
+				color[2] = 0;
+			}
+
+			/*cv::Vec3b color = image.at<cv::Vec3b>(cv::Point(iterX, iterY));*/
+		}
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	cv::Mat image = cv::imread(fileName, cv::IMREAD_COLOR);
+	cv::Mat image = cv::imread(IN_FILENAME, cv::IMREAD_COLOR);
 	if (!image.data) {
 		cerr << "error: could not open or find the image" << endl;
 		return EXIT_FAILURE;
 	}
 
-	/*namedWindow("Display window", cv::WINDOW_AUTOSIZE);
-	imshow("Display window", image);*/
+	namedWindow(DEFAULT_WIN_NAME, cv::WINDOW_AUTOSIZE);
+	imshow(DEFAULT_WIN_NAME, image);
+	cv::waitKey();
 
-	cout << "Modifying the image... " << endl;
+	modifyImage(image);
 
-	for (int row = 0; row < image.rows; row++) {
-		for (int col = 0; col < image.cols; col++) {
-			cv::Vec3b& color = image.at<cv::Vec3b>(row, col);
+	namedWindow(MODIFIED_WIN_NAME, cv::WINDOW_AUTOSIZE);
+	imshow(MODIFIED_WIN_NAME, image);
+	cv::waitKey();
 
-			if ((color[0] == 0) && (color[1] == 0) && (color[2] == 0)) {
-				color[0] = 30;
-				color[1] = 30;
-				color[2] = 255;
-			}
-
-			/*cv::Vec3b color = image.at<cv::Vec3b>(cv::Point(iterX, iterY));
-			
-			if ((color[0] == 0) && (color[1] == 0) && (color[2] == 0))
-			{
-				color[0] = 30;
-				color[1] = 30;
-				color[2] = 255;
-
-				image.at<cv::Vec3b>(cv::Point(iterX, iterY)) = color;
-			}*/
-		}
-	}
-
-	cout << "Writing image to the file... " << endl;
-
-	cv::imwrite("./rectangle_modified.png", image);
-
-	cv::waitKey(0);
+	cv::imwrite(OUT_FILENAME, image);
 
 	return EXIT_SUCCESS;
 }
